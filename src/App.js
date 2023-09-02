d224978e7b5653aae40ea8610fe8628203083731
Ethan Reynolds <ethan.t.reynolds@gmail.com>
2023-09-02 16:19:38 -0600

    finish vending machine

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+
 import VendingMachine from './VendingMachine';
 import Pop from './Pop';
 import Cheetos from './Cheetos';
